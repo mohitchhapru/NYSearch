415a41eb4d6c3e3ab1a13c4a37d450a8cc9b4399
mohitchhapru <chhaprumohit@gmail.com>
2018-05-26 21:43:22 -0700

    Created Users Endpoint to fetch all users

--- users-api/src/main/java/com/example/usersapi/UsersApiApplication.java
+++ users-api/src/main/java/com/example/usersapi/UsersApiApplication.java
@@ -1,20 +1,14 @@
 package com.example.usersapi;
 
+import com.example.usersapi.models.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableEurekaClient
-@RestController
 public class UsersApiApplication {
-	@RequestMapping("/")
-	public String home() {
-		return "some users";
-	}
-	public static void main(String[] args) {
-		SpringApplication.run(UsersApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(UsersApiApplication.class, args);
+    }
 }
---
+++ users-api/src/main/java/com/example/usersapi/models/User.java
@@ -0,0 +1,33 @@
+package com.example.usersapi.models;
+
+import lombok.*;
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor @NoArgsConstructor @Getter @Setter
+@Entity @Table(name = "USERS")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "FIRST_NAME")
+    private String firstName;
+
+    @Column(name = "LAST_NAME")
+    private String lastName;
+
+    @Column(name = "USER_NAME")
+    private String userName;
+
+    @Column(name = "PASSWORD")
+    private String password;
+
+    public User(String firstName, String lastName, String userName, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.userName = userName;
+        this.password = password;
+    }
+}
