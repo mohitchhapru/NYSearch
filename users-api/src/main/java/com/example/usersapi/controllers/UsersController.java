dacf6a9e744f1875f020f2a6ab3a42cac67f370b
mohitchhapru <chhaprumohit@gmail.com>
2018-05-26 21:59:54 -0700

    Implemented endpoint for deleting user

--- users-api/src/main/java/com/example/usersapi/controllers/UsersController.java
+++ users-api/src/main/java/com/example/usersapi/controllers/UsersController.java
@@ -3,6 +3,7 @@ package com.example.usersapi.controllers;
 import com.example.usersapi.models.User;
 import com.example.usersapi.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -26,5 +27,11 @@ public class UsersController {
     @PostMapping("/")
     public User createNewUser(@RequestBody User newUser) {
         return userRepository.save(newUser);
+    }
+
+    @DeleteMapping("/users/{userId}")
+    public HttpStatus deleteUserById(@PathVariable Long userId) {
+        userRepository.delete(userId);
+        return HttpStatus.OK;
     }
 }
